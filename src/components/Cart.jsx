e09bb050e32c4fcd4a6ba4e28373ba901c80e7bb
arunbaaby <arunarunvb0398@gmail.com>
2025-07-10 19:57:36 +0530

    Cart page suscription

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,22 @@
 import { useSelector } from "react-redux";
 
-const Cart = ()=>{
+const Cart = () => {
 
-    const cartItems = useSelector((store)=>store.cart.items);
+    const cartItems = useSelector((store) => store.cart.items);
+
     return (
-        <div className="cart-wrapper flex flex-col">
-            <h1 className="text-3xl font-bold">Cart</h1>
-            <h1 className="text-2xl">{cartItems}</h1>
+        <div className="grid grid-cols-3 gap-7 mx-4">
+            <div className="col-span-2 bg-white shadow-xl">
+
+            </div>
+            <div className="p-4 bg-white shadow-xl">
+                {cartItems.map((item, index) => (
+                    <div className="border-b-1 border-gray-400" key={index}>
+                        <div>{item.name}</div>
+                        <div>{item.defaultPrice || item.price}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }

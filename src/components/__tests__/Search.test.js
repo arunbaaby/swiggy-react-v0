f8475e2c25f9753dcee050b9893adb66ff76883f
arunbaaby <arunarunvb0398@gmail.com>
2025-07-26 17:48:34 +0530

    body test

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,16 +1,23 @@
 import '@testing-library/jest-dom';
-import {render} from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import Body from '../Body';
 import MOCK_DATA from '../mocks/mockResListData.json';
+import { BrowserRouter } from 'react-router-dom';
+// jest only renders the app not in a real browser so it can not fetch the data
+// so we have to define a mock fetch or
+// error= fetch is not defined (fetch is provided by real browser)
 
-global.fetch = jest.fn(()=>{
+global.fetch = jest.fn(() => {
     return Promise.resolve({
-        jest: ()=>{
+        json: () => {
             return Promise.resolve(MOCK_DATA);
         }
     })
 });
 
-it ("Should render the body with search",()=>{
-    render(<Body/>)
+it("Should render the body with search", async () => {
+    await act(async () => render(
+    <BrowserRouter>
+        <Body />
+    </BrowserRouter>))
 })
